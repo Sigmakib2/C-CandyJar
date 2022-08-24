63807ff3936aba512b7654f442f27ffeaf91a750
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2022-08-24 23:33:20 +0600

    1.3

---
+++ Programming in ANSI C Solutions/1.3 pattern of asterisks.c
@@ -0,0 +1,14 @@
+/*
+Write a program using one print statement to print the pattern of asterisks as shown below :
+*
+* *
+* * *
+* * * *
+*/
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    printf("* \n* * \n* * * \n* * * *");
+    getch();
+}
