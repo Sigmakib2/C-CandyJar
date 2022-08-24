a9e2d383bff085f58c02ba8b636a4fa3012e8d94
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2022-08-24 23:04:24 +0600

    Create 1.1 print your mailing address.c

---
+++ Programming in ANSI C Solutions/1.1 print your mailing address.c
@@ -0,0 +1,17 @@
+/*
+Programming in ANSI C Problem 1.1
+
+Write a program that will print your mailing address in the following form :
+First line: Name
+Second line: Door No, Street
+Third line: City, Pin code
+*/
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    printf("Name: Jhone Doe\n");
+    printf("Door No: 20, Street:Big Street\n");
+    printf("City: New Doe, Pin Code: 007");
+    getch();
+}
